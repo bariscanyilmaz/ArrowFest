b0253e090d7b545c1c9c1eefdb2e98d2cd14d967
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-02-04 16:28:37 +0300

    create increase and decrease walls

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public enum Operator { Sum, Sub, Div, Mul };
+
+public class Wall : MonoBehaviour
+{
+
+    [SerializeField]
+    private Operator _operator;
+
+    [SerializeField]
+    [Range(1, 100)]
+    private int _value;
+
+    [SerializeField]
+    private TMP_Text _wallText;
+
+    void Start()
+    {
+        _wallText.text = $"{GetOperatorChar(_operator)}{_value}";
+    }
+
+    public int Value => _value;
+    public Operator Operator => _operator;
+
+    private static string GetOperatorChar(Operator op)
+    {
+        return op switch
+        {
+            Operator.Mul => "x",
+            Operator.Sum => "+",
+            Operator.Sub => "-",
+            Operator.Div => "/",
+            _ => string.Empty
+        };
+    }
+}
+
+
